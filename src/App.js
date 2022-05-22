91107cd9482d044c0711e97c522a463ffa92aaba
Divyanshu <divyanshukalola88@gmail.com>
2022-05-22 18:54:54 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ import Home from "./components/home";
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <h1>Kaizen Infrastuctures</h1>
-        <h2>Comming soon ..</h2>
-      </header> */}
       <Home></Home>
 
       {/* <CommingSoon></CommingSoon> */}
