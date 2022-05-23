9dc58790080330cb75c10ef34e1cc4e7ed2eeae5
Divyanshu <divyanshukalola88@gmail.com>
2022-05-23 12:53:07 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./index.css";
 
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import CommingSoon from "./components/commingsoon";
+import CommingSoon from "./components/commingsoon";
 import Home from "./components/home";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CommingSoon></CommingSoon>} />
+          <Route path="/home" element={<Home></Home>} />
+        </Routes>
+      </BrowserRouter>
 
       {/* <CommingSoon></CommingSoon> */}
     </div>
--- src/components/commingsoon.js
+++ src/components/commingsoon.js
@@ -1,12 +1,28 @@
 import * as React from "react";
+import mainlogo from "./../static/img/logo2big.png";
 
 function CommingSoon() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Kaizen Infrastuctures</h1>
-        <h2>Comming soon ..</h2>
-      </header>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="row text-center mt-5">
+        <div className="col">
+          <img src={mainlogo} alt="logo" height={"150px"} />
+        </div>
+      </div>
+      <div className="row text-center mt-5">
+        <div className="col">
+          <h4>Comming Soon ...</h4>
+        </div>
+      </div>
     </div>
   );
 }
