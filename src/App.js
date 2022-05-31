b1d52bfb73dbead904cfa858e078d61ca60a4b4a
Divyanshu <divyanshukalola88@gmail.com>
2022-05-31 10:18:56 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./index.css";
 
-import React, { useState } from "react";
+import React from "react";
+
+// import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import CommingSoon from "./components/commingsoon";
--- src/components/news.js
+++ src/components/news.js
@@ -22,7 +22,7 @@ import Typography from "@mui/material/Typography";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import CardActions from "@mui/material/CardActions";
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 
 function News() {
   return (
