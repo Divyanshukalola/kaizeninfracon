6e0235ab5d6dc9c400da8b028d5fcc38c7a6710c
Divyanshu <divyanshukalola88@gmail.com>
2022-06-24 13:06:13 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // import CommingSoon from "./components/commingsoon";
-import Home1 from "./components/home";
+import Home from "./components/home";
 import About from "./components/about";
 import News from "./components/news";
 import Career from "./components/career";
@@ -19,7 +19,7 @@ import AdminProjEdit from "./components/admin/projedit";
 import Timeline from "./components/timeline";
 import { db } from "./firebase-config";
 import { collection, getDocs } from "firebase/firestore";
-import Home from "./components/home1";
+import Home1 from "./components/home1";
 import ReactLoading from "react-loading";
 
 import SlideSHow from "./components/slideshow";
